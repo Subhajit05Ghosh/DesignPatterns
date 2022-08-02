9bcae858e56e34a9124d400a57c852a61b428c3a
KT718UJ <speak.subhajitghosh@gmail.com>
2022-08-03 00:15:23 +0530

    First Commit

---
+++ design_principles/src/main/java/design_principles/SolidPrinciples/SREP/UserPersistenceService.java
@@ -0,0 +1,11 @@
+package design_principles.SolidPrinciples.SREP;
+
+//A separate class for handling persistence 
+public class UserPersistenceService {
+
+	private Store store = new Store();
+	
+	public void saveUser(User user) {
+		store.store(user);
+	}
+}
